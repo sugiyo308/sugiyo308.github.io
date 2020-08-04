a38057d39275bb2ccb5a999cf6997581da863666
sugiyo308 <54385397+sugiyo308@users.noreply.github.com>
2020-08-05 06:07:21 +0700

    Add files via upload

--- fetch.js
+++ fetch.js
@@ -1,50 +1,34 @@
-// fetch api dari domain lain
-// localhost:5050 mengambil data dari localhost:4000
+const charactersList = document.getElementById('provensi');
+let hpCharacters = [];
 
-const urlUserData = "./slb.json";
-//set url source yang akan diambil datanya
-// const urlMain = 'http://localhost:4000/'
-// if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
-//   const urlMain = 'http://localhost:4000/';
-// }
-// else if (location.hostname === "sugiyo308.github.io" )  {
-//   const urlMain = 'http://sugiyo308.github.io/';
-// } 
-// else {
-//  const urlMain = '';
-// }
-// const urlUserData = urlMain + 'userdata'
-
-
-
-// fetch user data
-fetch(urlUserData)
-  // then ambil response sebagai json yang telah di parse
-  .then(result => result.json())
-  // then tampilkan data pada halaman web.
-  .then(showData)
-
-// tampilkan data sebagai tabel
-function showData(data) {
-  const table = document.getElementById('data')
-
-  //table body
-  for (let user of data) {
-    const row = table.insertRow()
-    for (let key in user) {
-      const cell = row.insertCell()
-      const text = document.createTextNode(user[key])
-      cell.append(text)
+const loadCharacters = async () => {
+    try {
+        const res = await fetch('slb.json');
+        hpCharacters = await res.json();
+        displayCharacters(hpCharacters);
+    } catch (err) {
+        console.error(err);
     }
-  }
+};
 
-  // create table head
-  const thead = table.createTHead()
-  const row = thead.insertRow()
-  for (let key in data[0]) {
-    const th = document.createElement('th')
-    const text = document.createTextNode(key)
-    th.append(text)
-    row.append(th)
-  }
-}
+const displayCharacters = (characters) => {
+    const htmlString = characters
+        .map((character) => {
+            return `
+            <li class="character">
+                <h2>nama lembaga: ${character.lembaga}</h2>
+                <p>alamat: ${character.alamat}</p>
+                <p>kota: ${character.kota}</p>
+                <p>provinsi: ${character.provinsi}</p>
+                <p>kode pos: ${character.pos}</p>
+                <p>telepon: ${character.telepon}</p>
+                <p>email: ${character.email}</p>
+                <p>website: ${character.website}</p>
+            </li>
+        `;
+        })
+        .join('');
+    charactersList.innerHTML = htmlString;
+};
+
+loadCharacters();
